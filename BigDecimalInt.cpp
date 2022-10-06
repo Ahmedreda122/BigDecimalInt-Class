11741c505877444489be096f1d2aae8dd6433b48
Abdallah Hussein <abiza2013@gmail.com>
2022-10-06 15:48:40 +0200

    Testing...

--- BigDecimalInt.cpp
+++ BigDecimalInt.cpp
@@ -4,15 +4,15 @@ BigDecimalInt::BigDecimalInt(string decStr)
 {
 	// TODO: 000005656565 remove zeros until number bigger than 0 //done
 	// TODO: + / - remove signs in the begin of string //done
-	// TODO: use stoi 
+	// TODO: use stoi
 	// TODO: push the remaining into num vector
-	/* 
+	/*
 	TODO: use this old code
 	// removing the spaces from the text to the end of it, then returning a pointer to the beginning of the removed spaces
 	auto removed = remove(txt.begin(), txt.end(), ' ');
 	// Erasing the spaces from the end of the text
 	txt.erase(removed, txt.end());
-	*/ 
+	*/
 	int cursor = 0;
 	bool streak = true;
 	if (decStr[0] == '+' || decStr[0] == '-')
@@ -22,7 +22,7 @@ BigDecimalInt::BigDecimalInt(string decStr)
 		// Where 0 is the first character index, 1 is the number of characters we need to remove from that index.
 		decStr.erase(0, 1);
 	}
-	else if (!isdigit(decStr[0])
+	else if (!isdigit(decStr[0]))
 	{
 		cout << "Invalid String";
 		return;
@@ -45,5 +45,5 @@ BigDecimalInt::BigDecimalInt(string decStr)
 			streak = false;
 		}
 	}
-
 }
+//...^_^...//
---
+++ Main.cpp
@@ -0,0 +1,5 @@
+#include "BigDecimalInt.cpp"
+int main()
+{
+    BigDecimalInt("9");
+}
