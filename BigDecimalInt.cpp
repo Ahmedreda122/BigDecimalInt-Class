6036839121919a96bb5518f281837a7319d52633
Ahmedreda122 <ahmalolireda12@gmail.com>
2022-10-06 09:45:18 +0200

    Setup the project

---
+++ BigDecimalInt.cpp
@@ -0,0 +1,1 @@
+#include "BigDecimalInt.h"
---
+++ BigDecimalInt.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class BigDecimalInt
+{
+private:
+	vector<int> num;
+
+public:
+	BigDecimal
+};
